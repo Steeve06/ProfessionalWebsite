22fedf7fdf364cdbef8a5382b827f6708727ce9d
steeve mocto <amoct@students.kennesaw.edu>
2026-03-31 19:31:47 -0400

    frontend deploy

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,6 +1,6 @@
 // This checks if the app is running on Vercel. 
 // If not, it defaults to your local Django server.
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';;
 
 export const API_ENDPOINTS = {
     CONSULTATION: `${BASE_URL}/api/consultations/`,
