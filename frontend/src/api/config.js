2f1ca1bde100452c638c27d5e2c497001d1278ec
steeve mocto <amoct@students.kennesaw.edu>
2026-03-31 19:49:18 -0400

    chore: trigger vercel build with new settings

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,5 +1,6 @@
 // This checks if the app is running on Vercel. 
 // If not, it defaults to your local Django server.
+//
 const BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
 
 export const API_ENDPOINTS = {
